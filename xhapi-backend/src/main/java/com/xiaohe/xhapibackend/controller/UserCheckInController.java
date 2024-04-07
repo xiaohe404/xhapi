4aeb0fa9763009fc003fa1c1527c30327663b8fe
xiaohe404 <xiaohe404@qq.com>
2024-04-07 13:46:48 +0800

    使用 Redisson 为每日签到任务加锁

--- xhapi-backend/src/main/java/com/xiaohe/xhapibackend/controller/UserCheckInController.java
+++ xhapi-backend/src/main/java/com/xiaohe/xhapibackend/controller/UserCheckInController.java
@@ -9,6 +9,7 @@ import com.xiaohe.xhapibackend.exception.ThrowUtils;
 import com.xiaohe.xhapibackend.model.entity.UserCheckIn;
 import com.xiaohe.xhapibackend.service.UserCheckInService;
 import com.xiaohe.xhapibackend.service.UserService;
+import com.xiaohe.xhapibackend.utils.RedissonLockUtils;
 import com.xiaohe.xhapicommon.model.vo.UserVO;
 import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.transaction.annotation.Transactional;
@@ -29,24 +30,30 @@ public class UserCheckInController {
     @Resource
     private UserCheckInService userCheckInService;
 
+    @Resource
+    private RedissonLockUtils redissonLockUtils;
+
     @PostMapping("/daily")
     @Transactional(rollbackFor = Exception.class)
     public BaseResponse<Boolean> dailyCheckIn(HttpServletRequest request) {
         UserVO loginUser = userService.getLoginUser(request);
         Long userId = loginUser.getId();
-        QueryWrapper<UserCheckIn> queryWrapper = new QueryWrapper<>();
-        queryWrapper.eq("user_id", userId);
-        UserCheckIn userCheckIn = userCheckInService.getOne(queryWrapper);
-        if (ObjectUtils.isNotEmpty(userCheckIn)) {
-            throw new BusinessException(ErrorCode.OPERATION_ERROR, "签到失败,今日已签到");
-        }
-        userCheckIn = new UserCheckIn();
-        userCheckIn.setUserId(userId);
-        userCheckIn.setAddPoints(10L);
-        boolean dailyCheckInResult = userCheckInService.save(userCheckIn);
-        boolean addPointsResult = userService.addPoints(userId, userCheckIn.getAddPoints());
-        boolean result = dailyCheckInResult && addPointsResult;
-        ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR, "调用失败");
-        return ResultUtils.success(true);
+        String redissonLock = ("dailyCheckIn_" + loginUser.getUserAccount()).intern();
+        return redissonLockUtils.redissonDistributedLocks(redissonLock, () -> {
+            QueryWrapper<UserCheckIn> queryWrapper = new QueryWrapper<>();
+            queryWrapper.eq("user_id", userId);
+            UserCheckIn userCheckIn = userCheckInService.getOne(queryWrapper);
+            if (ObjectUtils.isNotEmpty(userCheckIn)) {
+                throw new BusinessException(ErrorCode.OPERATION_ERROR, "今日已签到");
+            }
+            userCheckIn = new UserCheckIn();
+            userCheckIn.setUserId(userId);
+            userCheckIn.setAddPoints(10L);
+            boolean dailyCheckInResult = userCheckInService.save(userCheckIn);
+            boolean addPointsResult = userService.addPoints(userId, userCheckIn.getAddPoints());
+            boolean result = dailyCheckInResult && addPointsResult;
+            ThrowUtils.throwIf(!result, ErrorCode.OPERATION_ERROR, "调用失败");
+            return ResultUtils.success(true);
+        }, "签到失败");
     }
 }
